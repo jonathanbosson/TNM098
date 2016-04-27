57c87c92140d5abb08988f24fae35570ae04fff6
jonathanbosson <jonathan.bosson@gmail.com>
2016-04-27 20:05:14 +0200

    lab3p2 done, result.txt has the comparison of the different textfiles

--- main.cpp
+++ main.cpp
@@ -25,22 +25,21 @@ int main()
 {
     unordered_map<string, int> hashTable;
 
-    string line;
     ifstream file;
     ofstream newfile;
-    vector<string> data;
     string word;
     string filePath = "data/01.txt";
     string destPath = "translated/01.txt";
     int textsRead = 1;
-    cout << "Open textfile data/0" << textsRead << ".txt" << endl;
+    cout << "Open textfile data/0" << textsRead << ".txt\n";
     file.open(filePath);
     newfile.open(destPath);
     int pos = 0;
+    int maxI = 0;
     size_t period;
 
 
-    //Open file and store every line in a vector
+    // Translate texts
     while (textsRead < 11) // t = 10
     {
         if (file.is_open() && newfile.is_open())
@@ -71,6 +70,7 @@ int main()
                 if (got == hashTable.end()) // word wasn't found, insert it into hashTable.
                 {
                     hashTable.insert(std::pair<string, int>(word, pos));
+                    if (pos > maxI) maxI = pos;
                     newfile << pos << " ";
                     if (period != std::string::npos)
                         newfile << "\n";
@@ -81,9 +81,9 @@ int main()
                 }
                 pos++;
             }
-        file.close();
-        newfile.close();
-        textsRead++;
+            file.close();
+            newfile.close();
+            textsRead++;
         }
         else {
             ostringstream os;
@@ -97,24 +97,92 @@ int main()
             if (textsRead == 10) {
                 destPath = "translated/10.txt";
                 filePath = "data/10.txt";
-                cout << "Open textfile data/" << textsRead << ".txt" << endl;
+                cout << "Open textfile data/" << textsRead << ".txt\n";
             } else
-                cout << "Open textfile data/0" << textsRead << ".txt" << endl;
+                cout << "Open textfile data/0" << textsRead << ".txt\n";
 
             file.open(filePath);
             newfile.open(destPath);
         }
     }
-    int maxi = 0;
-    for (auto& x: hashTable) {
-        if (maxi < x.second)
-            maxi = x.second;
+    cout << "\nUnique words in hashtable: " << hashTable.size() << "\n"
+    << "Maximum id: " << maxI << "\n";
 
-        //cout << x.second << " ";
+    // Compare translated texts    
+    string line, comparedLine;
+    ifstream comparedFile;
+    int readLines = 0; int atLine = 0; int numWords = 0;
+    filePath = "translated/01.txt";
+    newfile.open("result.txt");
+
+    if (!newfile.is_open()) return 0;
+    // need to loop it around all texts to compare everything
+    for (int t = 1; t < 11; ++t) {
+        ostringstream ns;
+        ns << "translated/0" << t << ".txt";
+        filePath = ns.str();
+
+        if (t == 10) {
+            filePath = "translated/10.txt";
+            newfile << "\n*** Compare textfile translated/" << t << ".txt ***\n\n";
+            cout << "Compare textfile translated/" << t << ".txt\n";
+        } else {
+            newfile << "\n*** Compare textfile translated/0" << t << ".txt ***\n\n";
+            cout << "Compare textfile translated/0" << t << ".txt\n";
+        }
+        file.open(filePath);
+        if (file.is_open()) {
+            atLine = 0; numWords = 0;
+            while (getline(file, line)) {
+                // check witha ll other text's lines to see how it compares
+                istringstream stream(line);
+                numWords = distance(istream_iterator<string>(stream), istream_iterator<string>());
+                atLine++;
+                for (int i = 1; i < 11; ++i) {
+                    if (i == t) continue; // skip comparing a text with itself
+                    ostringstream cs;
+                    cs << "translated/0" << i << ".txt";
+                    destPath = cs.str();
+                    if (i == 10) destPath = "translated/10.txt";
+                    comparedFile.open(destPath);
+                    if (comparedFile.is_open()) {
+                        while (getline(comparedFile, comparedLine)) {
+                            readLines++;
+                            if (numWords > 1 && comparedLine == line) {
+                                if (i == 10)
+                                    newfile << "Line " << atLine << " is similar to " << i << ".txt at line " << readLines << "\n";
+                                else 
+                                    newfile << "Line " << atLine << " is similar to 0" << i << ".txt at line " << readLines << "\n";
+
+                                newfile << "\"";
+                                istringstream stream(line);
+                                for (string currWord; stream >> currWord; ) {
+                                    for (auto it = hashTable.begin(), end = hashTable.end(); 
+                                        it != end; ++it) {
+                                        if (it->second == stoi(currWord)){
+                                            newfile << it->first << " ";
+                                            break;
+                                        }
+                                    }
+                                }
+                                //newfile << line << " ";    
+                                newfile << "\"\n\n";
+
+                                // can't decode since we have a map data structure, can only find Key (string word)
+                                // hashTable.find(currWord)->first
+                                // make an estimate on how similar it is? better if statement?
+                            }
+                        }
+                        comparedFile.close();
+                        readLines = 0;
+                    }
+                }
+            }
+            file.close();
+        }
     }
+    newfile.close();
 
-    cout << endl << "Unique words in hashtable: " << hashTable.size() << endl
-    << "Maximum id: " << maxi;
 
 
     return 0;
